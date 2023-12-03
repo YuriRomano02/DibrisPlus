5db83289d773d904e546bc008f4f788c28e5636e
Samu1538 <samuel.universita@gmail.com>
2023-12-03 18:14:05 +0100

    cambiato il login

--- site/login/login.php
+++ site/login/login.php
@@ -3,18 +3,20 @@
 $mysqli = mysqli_connect('localhost', 'Utente', '1234', 'users');
 
 if ($mysqli->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-if(isset($_COOKIE["email"])){
+if (isset($_COOKIE["email"])) {
     $email = $_COOKIE["email"];
     $result = $conn->query("SELECT * FROM utenti WHERE email = '$email'");
-    if($result->num_rows > 0){
+    if ($result->num_rows > 0) {
         session_start();
         $_SESSION['user'] = $result->fetch_assoc();
-        if($email == "lollo02@gmail.com") header("Location: ../welcome/welcomeroot.html");
+        if ($email == "lollo02@gmail.com")
+            header("Location: ../welcome/welcomeroot.html");
         else {
-            header("Location: ../welcome/welcomebase.html");}
+            header("Location: ../welcome/welcomebase.html");
+        }
         exit;
     }
 }
@@ -22,22 +24,20 @@ if(isset($_COOKIE["email"])){
 if (isset($_POST['Email']) && isset($_POST['password'])) {
     $email = $_POST['Email'];
     $password = $_POST['password'];
-    $result= $mysqli->query("SELECT * FROM utenti WHERE email = '$email'");
+    $result = $mysqli->query("SELECT * FROM utenti WHERE email = '$email'");
 
-    if(isset($_POST['remember'])) {
+    if (isset($_POST['remember'])) {
         $Cookie_email = "email";
         $Cookie_password = "password";
-        setcookie($Cookie_email, $email, time() + (86400 * 30), "/"); 
-        setcookie($Cookie_password, $password, time() + (86400 * 30), "/"); 
-    }
-    else{
-        setcookie("username","");
-        setcookie("password","");
+        setcookie($Cookie_email, $email, time() + (86400 * 30), "/");
+        setcookie($Cookie_password, $password, time() + (86400 * 30), "/");
+    } else {
+        setcookie("username", "");
+        setcookie("password", "");
     }
 
-    if(isset($_POST['log_out']))
-    {
-        if(session_status() == PHP_SESSION_NONE) {
+    if (isset($_POST['log_out'])) {
+        if (session_status() == PHP_SESSION_NONE) {
             session_start();
         }
 
@@ -45,9 +45,9 @@ if (isset($_POST['Email']) && isset($_POST['password'])) {
 
         session_destroy();
 
-        if(isset($_COOKIE["email"]) AND isset($_COOKIE["password"])){
-            setcookie("email", '', time() - (86400 * 30),'/');
-            setcookie("password", '', time() - (86400 * 30),'/');
+        if (isset($_COOKIE["email"]) and isset($_COOKIE["password"])) {
+            setcookie("email", '', time() - (86400 * 30), '/');
+            setcookie("password", '', time() - (86400 * 30), '/');
         }
 
         header('Location:login.php');
@@ -59,9 +59,8 @@ if (isset($_POST['Email']) && isset($_POST['password'])) {
         if (!password_verify($password, $row['password'])) {
             echo "<script>alert('Password is incorrect.');window.location.href='login.php';</script>";
             exit();
-        }
-        else{
-        $_SESSION['email'] = $email;
+        } else {
+            $_SESSION['email'] = $email;
             if ($email == "lollo02@gmail.com") {
                 header("Location: ../welcome/welcomeroot.html");
             } else {
@@ -71,7 +70,7 @@ if (isset($_POST['Email']) && isset($_POST['password'])) {
         }
     }
 
-    
+
 }
 
 $mysqli->close();
@@ -79,32 +78,64 @@ $mysqli->close();
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.cdnfonts.com/css/new-walt-disney-font" rel="stylesheet">
 </head>
-<body>
-    <form action="login.php" method="post">
-        <fieldset class="bordo">
-        <legend>Credenziali da inserire</legend>
-        <img src="Dibris.png" alt="">
-        <label for="email" class="email">Email</label><br >
-        <input type="email" placeholder="enter mail" id="email" name="Email" value="<?php if(isset($_COOKIE['email'])) { echo $_COOKIE['email']; } ?>" required><br>
-        <label for="password" class="pass">password</label><br>
-        <input type="password" placeholder="enter password" id="password" maxlength="24" minlength="6" name="password" value="<?php if(isset($_COOKIE['password'])) { echo $_COOKIE['password']; } ?>" required><br>
-        <label for="remember">Ricordami</label><br >
-        <input type="checkbox" id="remember" name="remember">
-        <div>
-            <p>Hai dimenticato la password? <a href="../registration/info.html">Clicca per maggiori informazioni</a>.</p>
-            <p>Account non ancora registrato? <a href="../registration/registration.html">Effettua la registrazione</a></p>
-            <button type="submit" name="log_out">logout</button>
-        </div>
-        </fieldset>
-        <button type="submit" name="submit" class="registerbtn">SUBMIT</button>
-    </form>
-    <?php echo ""?>
-</body>
-</html>
 
+<body>
+
+
+<section class="sticky">
+  <div class="bubbles">
+      <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    <div class="bubble"></div>
+    
+  </div>
+</section>
+
+
+    <div id = "div_logo">
+        <p>Dibris</p>
+        <p id="stellina">+</p>
+    </div>
+
+
+    <form action="login.php" method="post">
+            <label for="email" class="email">Email</label><br>
+            <input type="email" placeholder="enter mail" id="email" name="Email"
+                value="<?php if (isset($_COOKIE['email'])) {
+                    echo $_COOKIE['email'];
+                } ?>" required><br>
+
+            <label for="password" class="pass">password</label><br>
+            <input type="password" placeholder="enter password" id="password" maxlength="24" minlength="6"
+                name="password" value="<?php if (isset($_COOKIE['password'])) {
+                    echo $_COOKIE['password'];
+                } ?>"
+                required><br>
+
+            <label for="remember">Ricordami</label><br>
+            <input type="checkbox" id="remember" name="remember">
+
+            <div>
+                <p><a href="../registration/info.html">Hai dimenticato la password?</a></p>
+                <p><a href="../registration/registration.html">Account non ancora registrato?</a></p>
+            </div>
+        <button type="submit" name="submit" class="registerbtn">USBMIT</button>
+    </form>
+</body>
+
+</html>
