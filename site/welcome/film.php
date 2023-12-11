4ef96a94e3774d33f7299173d573e543f4c94ac7
Samu1538 <samuel.universita@gmail.com>
2023-12-11 01:01:28 +0100

    aggiornata la pagina film

--- site/welcome/film.php
+++ site/welcome/film.php
@@ -21,29 +21,67 @@
     include "../Elementi in comune/sfondo.html";
     include "../Elementi in comune/sidebar.php";
 
+    include "./databaseConnection.php";
+    $query = "SELECT * FROM film";
+    $result = $mysqli->query($query);
+    $row = $result->fetch_assoc();
+    $prova = "../Immagini e gif/Immagini/locandine/The Marvels.jpg";
     ?>
 
     <div class="elementi">
 
-        <h1>Nome film</h1><i class="fa-regular fa-heart preferiti"></i><br>
+        <h1>
+            <?php echo $row["Titolo"] ?>
+        </h1><i class="fa-regular fa-heart preferiti"></i><br>
 
 
 
         <div class="informazioni">
-            <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=MJb0qPYp7nhe8moR&rel=0"
-                title="YouTube video player" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen></iframe>
+            <div class="video-container">
+                <iframe width="560" height="315"
+                    src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=MJb0qPYp7nhe8moR&rel=0"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+            </div>
 
-            <img src="immagine del film">
+            <img src="<?php echo $row["Locandina"] ?>" alt="<?php echo $row["Locandina"] ?>">
         </div>
 
         <div class="informazioni">
-            <div>
-                Descrizione del film
+            <div class="contenitori">
+                <?php echo $row["Descrizione"] ?>
             </div>
-            <div>
-                Informazioni
+            <div class="contenitori">
+                <ul>
+                    <li>Data di rilascio:
+                        <?php echo $row["AnnoDiRilascio"] ?>
+                    </li>
+                    <li>Regista:
+                        <?php echo $row["Regista"] ?>
+                    </li>
+                    <li>Generi:
+                        <?php echo $row["Genere"] ?>
+                    </li>
+                    <li>Durata:
+                        <?php echo $row["Durata"] ?> minuti
+                    </li>
+                    <li>Produzione:
+                        <?php echo $row["Produzione"] ?>
+                    </li>
+                    <li>Distribuzione:
+                        <?php echo $row["Distribuzione"] ?>
+                    </li>
+                    <li>Paese:
+                        <?php echo $row["Paese"] ?>
+                    </li>
+                    <li>Incassi:
+                        <?php echo $row["Incassi"] ?>
+                    </li>
+                    <li>Costi di produzione:
+                        <?php echo $row["CostiDiProduzione"] ?>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
---
+++ site/welcome/film2.php
@@ -0,0 +1,6 @@
+<?php
+include "./databaseConnection.php";
+$query = "SELECT * FROM film";
+$result = $mysqli->query($query);
+
+?>
