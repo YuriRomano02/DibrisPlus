8fa6f29dc5fbef1d8d04ef86d254cd63ddb2d594
Samu1538 <samuel.universita@gmail.com>
2023-12-18 15:01:11 +0100

    spostato allusers.php

--- site/Elementi in comune/sidebar.php
+++ site/Elementi in comune/sidebar.php
@@ -3,7 +3,7 @@
     <a href="../Home/home.php"><i class="fa-solid fa-house icona"></i></a>
     <a href="../Cinema/cinema.php"><i class="fa-solid fa-film icona"></i></a>
     <a href="../Search page/search_page.php"><i class="fa-solid fa-magnifying-glass icona"></i></a>
-    <?php
+    <?php 
         session_start();
         $email = 'S5231931@studenti.unige.it';
         if(isset($_SESSION['email']) && $_SESSION['email'] == $email) {
@@ -11,6 +11,5 @@
         }
     ?>
     <a href="../Area utente/user.php"><i class="fa-regular fa-user icona"></i></a>
-    
     <a href="../exit.php"><i class="fa-solid fa-right-from-bracket icona"></i></a>
 </aside>
