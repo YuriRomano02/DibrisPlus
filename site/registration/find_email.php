1a4aa74e0a8a6912ad36ebb32529d999f20b3526
Samu1538 <samuel.universita@gmail.com>
2024-01-13 17:37:26 +0100

    UPDATE: registration.js

--- site/registration/find_email.php
+++ site/registration/find_email.php
@@ -2,13 +2,17 @@
 include "../Elementi in comune/databaseConnection.php";
 
 $query = "SELECT Email FROM Utenti WHERE Email = ?";
+
 $stmt = $conn->prepare($query);
 $stmt->bind_param("s", $_POST["email"]);
 $stmt->execute();
 $result = $stmt->get_result();
-
-if ($result->num_rows > 0) {
-    return false;
+$prova = [];
+while ($row = $result->fetch_assoc()) {
+    array_push($prova, $row);
 }
+
+echo json_encode($prova);
+
 
 ?>
--- site/registration/registration.js
+++ site/registration/registration.js
@@ -4,23 +4,44 @@ const lastname = document.getElementById("lastname")
 const email = document.getElementById("email")
 const pass = document.getElementById("pass")
 const confirmPass = document.getElementById("confirm")
+const emailEsistente = document.getElementById("emailEsistente")
 
 function controlloPassword() {
-    return pass.value == confirm.value
+    return pass.value == confirmPass.value
 }
 
 function campiVuoti() {
     return (!firstname.value || !lastname.value || !email.value || !pass.value || !confirmPass.value)
 }
 
-function controlloEmail(){
-
-}
-
-email.addEventListener("change", function(){
+email.addEventListener("change", function (event) {
+    datiForm = new FormData()
+    datiForm.append("email", event.target.value)
+    fetch("./find_email.php", {
+        method: "POST",
+        body: datiForm
+    })
+        .then(response => response.json())
+        .then(data => {
+            if(data){
+                emailEsistente.innerText = "email già in uso"
+                emailEsistente.style.color = "red"
+            }{
+                emailEsistente.innerText = "email"
+                emailEsistente.style.color = "cyan"
+            }
+        })
 })
 
 form.addEventListener("submit", function (event) {
+    datiForm = new FormData()
+
+    datiForm.append("firstname", event.target.firstname.value)
+    datiForm.append("lastname", event.target.lastname.value)
+    datiForm.append("email", event.target.email.value)
+    datiForm.append("pass", event.target.pass.value)
+    datiForm.append("confirm", event.target.confirmPass.value)
+
     event.preventDefault()
     if (campiVuoti()) {
         alert("Inserisci tutti i campi obbligatori")
@@ -29,7 +50,7 @@ form.addEventListener("submit", function (event) {
             if (!form.elements[i].value) {
                 form.elements[i].style.background = "#ffd37a"
             }
-            else{
+            else {
                 form.elements[i].style.background = "white"
             }
         }
@@ -37,5 +58,12 @@ form.addEventListener("submit", function (event) {
     else if (!controlloPassword()) {
         alert("La password non coincide con la conferma della password " + form.elements[0].value)
         confirmPass.value = ""
+    }
+    else {
+        fetch("./registration.php", {
+            method: "POST",
+            body: datiForm
+        })
+            .then(response => console.log(response))
     }
 })
