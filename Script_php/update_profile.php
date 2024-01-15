08da4e22408313d3cde6c0d1eb9202ad46c5aebc
Yuri Romano <103211870+sassogrosso@users.noreply.github.com>
2024-01-15 15:04:40 +0100

    per me

--- Script_php/update_profile.php
+++ Script_php/update_profile.php
@@ -1,43 +1,28 @@
 <?php
 
 if (isset($_POST["submit"])) {
-    if (isset($_FILES["foto"]["tmp_name"])) {
-        $b = getimagesize($_FILES["foto"]["tmp_name"]);
-        $email = $_SESSION['email'];
-        if ($b !== false) {
-            //Get the contents of the image
-            $file = $_FILES['foto']['tmp_name'];
-            $image = addslashes(file_get_contents($file));
-            $nome = $_POST['Nome'];
-            $cognome = $_POST['Cognome'];
-            $cell = $_POST['cell'];
+
+    $filename = $_FILES["foto"]["name"];
+    $tempname = $_FILES["foto"]["tmp_name"];
+    $folder = "./images/" . $filename;
+
+    $nome = $_POST['Nome'];
+    $cognome = $_POST['Cognome'];
+    $cell = $_POST['cell'];
 
 
-            $host = 'localhost';
-            $username = 'yuri';
-            $password = 'romanus99';
-            $db = 'unige';
+    include "../Common_elements/databaseConnection.php";
 
-            $db = new mysqli($host, $username, $password, $db);
+    $query = "UPDATE utenti SET photo = '$filename', nome = '$nome', cognome = '$cognome', numero_telefono = '$cell' WHERE email = '$email'";
 
-            // Check if the connection was successful
-            if ($db->connect_error) {
-                die("Connection failed: " . $db->connect_error);
-            }
+    $result = $conn->query($query);
 
-            $query = "UPDATE utenti SET photo = '$image', nome = '$nome', cognome = '$cognome', numero_telefono = '$cell' WHERE email = '$email'";
-
-            $result = $db->query($query);
-
-            if ($result === true) {
-                echo "Photo updated successfully";
-                header("Location: ./show_profile.php");
-            } else {
-                echo "Error updating photo: " . $db->error;
-            }
-
-            $db->close();
-        }
+    if ($result === true) {
+        echo "Photo updated successfully";
+        header("Location: ./show_profile.php");
+    } else {
+        echo "Error updating photo: " . $conn->error;
     }
+
+    $conn->close();
 }
-?>
