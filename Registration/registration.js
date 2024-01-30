66e62c1eb53831aa47b235dac8f7027cde77f473
Samu1538 <samuel.universita@gmail.com>
2024-01-30 22:48:14 +0100

    migliorato il js

--- Registration/registration.js
+++ Registration/registration.js
@@ -1,18 +1,5 @@
-const form = document.getElementById("form")
-const firstname = document.getElementById("firstname")
-const lastname = document.getElementById("lastname")
 const email = document.getElementById("email")
-const pass = document.getElementById("pass")
-const confirmPass = document.getElementById("confirm")
 const emailEsistente = document.getElementById("emailEsistente")
-
-function controlloPassword() {
-    return pass.value == confirmPass.value
-}
-
-function campiVuoti() {
-    return (!firstname.value || !lastname.value || !email.value || !pass.value || !confirmPass.value)
-}
 
 email.addEventListener("change", function (event) {
     datiForm = new FormData()
